0044693decb2e5da6cd8fea5e8b48ed19139e311
RitikChauhan1704 <critik1704@gmail.com>
2025-06-28 23:31:56 +0530

    s

---
+++ src/app/api/ai-interview/route.ts
@@ -0,0 +1,16 @@
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+
+const model = new ChatGoogleGenerativeAI({
+  model: "gemini-2.0-flash",
+  temperature: 0,
+});
+
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+
+const messages = [
+  new SystemMessage("Translate the following from English into Italian"),
+  new HumanMessage("hi!"),
+];
+
+await model.invoke(messages);
+
